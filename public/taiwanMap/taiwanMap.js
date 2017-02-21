33c81eb6f06cac97063ad5ae3db48db8fe74f64c
林雨柔 <mirrorLin@doitsky.com>
2017-02-21 19:57:17 +0800

    TaiwanMap 整理資料與畫圖中

--- public/taiwanMap/taiwanMap.js
+++ public/taiwanMap/taiwanMap.js
@@ -6,14 +6,180 @@ function init() {
 }
 
 function getCityAvgSleepTime(){
-    var whereCondition = {
-        address: "台北市"
-    };
+    var urlQuery = {
+        totalOrAvg: "avg",
+        totalSleepTimeOrSleepInTime: "totalSleepTime"
+    }; 
+    var allMemberSleepTimes = getAggregateSleeptimeData(urlQuery);
     
-    var membersInTaipei_sleepTimes = getMemberAndSleeptimesData(whereCondition);
-    console.log(membersInTaipei_sleepTimes);
-    var membersInTaipei_sleepTimes_map = _.mapValues(membersInTaipei_sleepTimes,function(obj) { return obj.sleepTimes; });
-    //console.log(membersInTaipei_sleepTimes_map)
+    var sleepTimeMiliseconds = getSleepTimeMilisecondsInEachCity(allMemberSleepTimes);
+    var avgSleepTimeInEachCityArr = getSleepTimeHoursInEachCity(sleepTimeMiliseconds);
+    //console.log(avgSleepTimeInEachCityArr)
+}
+
+function getSleepTimeHoursInEachCity(sleepTimeMiliseconds){
+    //var citys = _.keys(sleepTimeMiliseconds);
+    var taipei_sleepTimeHrs = "";//台北市
+    var newTaipeiCity_sleepTimeHrs = "";//新北市
+    var taoyuan_sleepTimeHrs = "";//桃園市
+    var taichungCity_sleepTimeHrs =[];//台中市:54
+    var tainanCity_sleepTimeHrs =[];//台南市:16
+    var kaushungCity_sleepTimeHrs =[];//高雄市:46
+    var keelungCity_sleepTimeHrs =[];//基隆市:9
+    var xinZhuCity_sleepTimeHrs =[];//新竹市:10
+    var jiaYiCity_sleepTimeHrs =[];//嘉義市:10
+    var xinZhu_sleepTimeHrs =[];//新竹縣:15
+    var miaoLi_sleepTimeHrs =[];//苗栗縣:21
+    var zhangHau_sleepTimeHrs =[];//彰化縣:9
+    var nangtou_sleepTimeHrs =[];//南投縣
+    var yunLin_sleepTimeHrs =[];//雲林縣
+    var jiaYi_sleepTimeHrs =[];//嘉義縣
+    var pingDong_sleepTimeHrs =[];//屏東縣
+    var yiLan_sleepTimeHrs =[];//宜蘭縣
+    var hauLian_sleepTimeHrs =[];//花蓮縣
+    var taiDong_sleepTimeHrs =[];//台東縣
+    var pongHu_sleepTimeHrs =[];//澎湖縣
+
+    var avgSleepTimeInEachCityArr = [];
+
+    sleepTimeMiliseconds.map(function(aCity,index){
+        var cityName = _.keys(aCity)[0];
+        var citySleepTime= _.values(aCity)[0];
+
+        var avgSleepTimeInACityMiliSeconds = Math.round(_.sum(citySleepTime)/citySleepTime.length);
+        var avgSleepTimeInHours = millisecondToDate(avgSleepTimeInACityMiliSeconds);
+        
+        var object = {
+            city: cityName,
+            avgSleepTime_Hours: avgSleepTimeInHours
+        }
+        avgSleepTimeInEachCityArr.push(object);
+    });
+    return avgSleepTimeInEachCityArr;
+}
+
+function getSleepTimeMilisecondsInEachCity(allMemberSleepTimes){
+    var sleepTimesInTaipeiCity=[];//台北市:105
+    var sleepTimesInNewTaipeiCity=[];//新北市:53
+    var sleepTimesInTaoyuanCity=[];//桃園市:22
+    var sleepTimesInTaichungCity=[];//台中市:54
+    var sleepTimesInTainanCity=[];//台南市:16
+    var sleepTimesInKaushungCity=[];//高雄市:46
+    var sleepTimesInKeelungCity=[];//基隆市:9
+    var sleepTimesInXinZhuCity=[];//新竹市:10
+    var sleepTimesInJiaYiCity=[];//嘉義市:10
+    var sleepTimesInXinZhu=[];//新竹縣:15
+    var sleepTimesInMiaoLi=[];//苗栗縣:21
+    var sleepTimesInZhangHau=[];//彰化縣:9
+    var sleepTimesInNangtou=[];//南投縣
+    var sleepTimesInYunLin=[];//雲林縣
+    var sleepTimesInJiaYi=[];//嘉義縣
+    var sleepTimesInPingDong=[];//屏東縣
+    var sleepTimesInYiLan=[];//宜蘭縣
+    var sleepTimesInHauLian=[];//花蓮縣
+    var sleepTimesInTaiDong=[];//台東縣
+    var sleepTimesInPongHu=[];//澎湖縣
+    var catchError=[];
+
+    for(var i=0;i<allMemberSleepTimes.length;i++){
+        var memberAddress = allMemberSleepTimes[i].member[0].address;
+        var sleepTime = allMemberSleepTimes[i].sleepTime;
+        
+        var object = {
+            city: memberAddress,
+            sleepTime: allMemberSleepTimes[i].sleepTime
+        }
+        
+        switch(memberAddress){
+            case '台北市':
+                sleepTimesInTaipeiCity.push(sleepTime)
+                break;
+            case '新北市':
+                sleepTimesInNewTaipeiCity.push(sleepTime)
+                break;
+            case '桃園市':
+                sleepTimesInTaoyuanCity.push(sleepTime)
+                break;
+            case '台中市':
+                sleepTimesInTaichungCity.push(sleepTime)
+                break;
+            case '台南市':
+                sleepTimesInTainanCity.push(sleepTime)
+                break;
+            case '高雄市':
+                sleepTimesInKaushungCity.push(sleepTime)
+                break;
+            case '基隆市':
+                sleepTimesInKeelungCity.push(sleepTime)
+                break;
+            case '新竹市':
+                sleepTimesInXinZhuCity.push(sleepTime)
+                break;
+            case '嘉義市':
+                sleepTimesInJiaYiCity.push(sleepTime)
+                break;
+            case '新竹縣':
+                sleepTimesInXinZhu.push(sleepTime)
+                break;
+            case '苗栗縣':
+                sleepTimesInMiaoLi.push(sleepTime)
+                break;
+            case '彰化縣':
+                sleepTimesInZhangHau.push(sleepTime)
+                break;
+            case '南投縣':
+                sleepTimesInNangtou.push(sleepTime)
+                break;
+            case '雲林縣':
+                sleepTimesInYunLin.push(sleepTime)
+                break;
+            case '嘉義縣':
+                sleepTimesInJiaYi.push(sleepTime)
+                break;
+            case '屏東縣':
+                sleepTimesInPingDong.push(sleepTime)
+                break;
+            case '宜蘭縣':
+                sleepTimesInYiLan.push(sleepTime)
+                break;
+            case '花蓮縣':
+                sleepTimesInHauLian.push(sleepTime)
+                break;
+            case '台東縣':
+                sleepTimesInTaiDong.push(sleepTime)
+                break;
+            case '澎湖縣':
+                sleepTimesInPongHu.push(sleepTime)
+                break;
+            default:
+                catchError.push(sleepTime)
+                break;
+        }
+    }
+
+    var sleepTimeMiliseconds = [
+        {'台北市': sleepTimesInTaipeiCity},
+        {'新北市': sleepTimesInNewTaipeiCity},
+        {'桃園市': sleepTimesInTaoyuanCity},
+        {'台中市': sleepTimesInTaichungCity},
+        {'台南市': sleepTimesInTainanCity},
+        {'高雄市': sleepTimesInKaushungCity},
+        {'基隆市': sleepTimesInKeelungCity},
+        {'新竹市': sleepTimesInXinZhuCity},
+        {'嘉義市': sleepTimesInJiaYiCity},
+        {'新竹縣': sleepTimesInXinZhu},
+        {'苗栗縣': sleepTimesInMiaoLi},
+        {'彰化縣': sleepTimesInZhangHau},
+        {'南投縣': sleepTimesInNangtou},
+        {'雲林縣': sleepTimesInYunLin},
+        {'嘉義縣': sleepTimesInJiaYi},
+        {'屏東縣': sleepTimesInPingDong},
+        {'宜蘭縣': sleepTimesInYiLan},
+        {'花蓮縣': sleepTimesInHauLian},
+        {'台東縣': sleepTimesInTaiDong},
+        {'澎湖縣': sleepTimesInPongHu},
+    ];
+    return sleepTimeMiliseconds;
 }
 
 function render(){
@@ -36,6 +202,23 @@ function render(){
                 .attr("d", path)
                 .attr('fill','green')
                 .on("click", clicked);
+                // .on("click",function(d){
+                //     console.log(this)
+                    // var itemCX = d3.select(this).attr("cx");
+                    // var itemCY = d3.select(this).attr("cy");
+                    // var tooltip = d3.select("#tooltip")
+                    //                 .style({
+                    //                     left: +itemCX+20+"px",
+                    //                     top: +itemCY+20+"px"
+                    //                 })
+                    // tooltip.select('#city')//.text(d.city);
+                    // tooltip.select('#industry')//.text(d.industry);
+                    // d3.select('#tooltip').classed('hidden',false);
+                //})
+                // .on("mouseout",function(d){
+                //     d3.select('#tooltip').classed('hidden',true);
+                // })  
+
         
         g.append("path").datum(
             topojson.mesh(
@@ -44,12 +227,15 @@ function render(){
                 (a,b)=>{
                     return a !== b;
             })
-        ).attr("id", "borders").attr("d", path);   
+        )
+        .attr("id", "borders")
+        .attr("d", path);
+
     })
 
     function clicked(d){
         var x, y, k;
-
+        
         if (d && centered !== d) {
             
             var centroid = path.centroid(d);
@@ -57,6 +243,18 @@ function render(){
             y = centroid[1];
             k = 4;
             centered = d;
+            console.log(this,'x:'+x,'y:'+y)
+
+            var itemX = x;
+            var itemY = y;
+
+            var tooltip = d3.select("#tooltip").style({
+                right: +itemX-250+"px",
+                top: +itemY+"px"
+            });
+            tooltip.select('#city');
+            tooltip.select('#industry');
+            d3.select('#tooltip').classed('hidden',false);
         } else {
             x = width / 2;
             y = (height / 2);
@@ -71,5 +269,10 @@ function render(){
             .duration(1000)
             .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
             .style("stroke-width", 1.5 / k + "px");
+
+
+        // .append("title").text(function(d){
+        //     return d.city + "\r\n" + d.industry;
+        // })    
      }
 }
--- public/util.js
+++ public/util.js
@@ -71,3 +71,27 @@ function isOverTwelve(hour){
 function getCurrentDate(date){
     
 }
+
+/*
+    將 "毫秒" 轉 "時/分/秒"
+    
+*/
+function millisecondToDate(msd){
+    var time = parseFloat(msd) / 1000;
+    if (null != time && "" != time) {
+        if (time > 60 && time < 60 * 60) {
+            time = parseInt(time / 60.0) + "分鐘" + parseInt((parseFloat(time / 60.0) -
+                parseInt(time / 60.0)) * 60) + "秒";
+        }
+        else if (time >= 60 * 60 && time < 60 * 60 * 24) {
+            time = parseInt(time / 3600.0) + "小時" + parseInt((parseFloat(time / 3600.0) -
+                parseInt(time / 3600.0)) * 60) + "分鐘" +
+                parseInt((parseFloat((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) -
+                parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60)) * 60) + "秒";
+        }
+        else {
+            time = parseInt(time) + "秒";
+        }
+    }
+    return time;
+} 
